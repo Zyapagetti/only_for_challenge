b05996d2705059844d72f9747c78a8e55ddcd3ea
Zyapagetti <jeff9435@gmail.com>
2021-08-31 18:43:58 +0900

    momentum

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const numGeneratorInput = document.querySelector("#num-generator__form input");
-const numGuessInput = document.querySelector("#num-guess__form input");
+const numGeneratorInput = document.querySelector("num-generator__input");
+const numGuessInput = document.querySelector("#num-guess__input");
 const numGuessButton = document.querySelector("#num-guess__form button");
 
 const result = document.querySelector("#result");
 
 function setNumRange(event) {
   event.preventDefault();
+  const machineNum = parseInt(numGeneratorInput.value);
+  const myNum = parseInt(numGuessInput.value);
+  const randNum = Math.ceil(Math.random() * machineNum);
+  result.innerText = `You chose ${myNum} . / The machine chose ${machieNum} .`;
+  console.log(randNum);
+  console.log(myNum);
 }
 
-numGuessInput.addEventListener("click", setNumRange);
+numGuessInput.addEventListener("submit", setNumRange);
