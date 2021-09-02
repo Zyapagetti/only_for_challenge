a464c664840bed6e4b80f862e76a48f0e80a70fa
Zyapagetti <jeff9435@gmail.com>
2021-09-02 12:34:19 +0900

    momentum RandBG challenge

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-const clock = document.querySelector("#clock");
-const christmas = document.querySelector("#day-left");
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
 
-function getClock() {
-  const date = new Date();
-  const year = String(date.getFullYear());
-  const month = String(date.getMonth()).padStart(2, "0");
-  const day = String(date.getDay()).padStart(2, "0");
-  const hour = String(date.getHours()).padStart(2, "0");
-  const minute = String(date.getMinutes()).padStart(2, "0");
-  const second = String(date.getSeconds()).padStart(2, "0");
-  clock.textContent = `현재시간 : ${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분 ${second}초`;
+const button = document.querySelector("button");
 
-  const goalDate = new Date("December 25, 2021 00:00:00");
-  const decimalDate = Math.floor((goalDate - date) / 1000);
-  const decimalDay = String(Math.floor(decimalDate / (60 * 60 * 24)));
-  const decimalHour = String(Math.floor((decimalDate / 3600) % 24)).padStart(
-    2,
-    "0"
-  );
-  const decimalMinute = String(Math.floor((decimalDate / 60) % 60)).padStart(
-    2,
-    "0"
-  );
-  const decimalSecond = String((decimalDate % 60) + 1).padStart(2, "0");
-  christmas.textContent = `남은시간 : ${decimalDay}일 ${decimalHour}시간 ${decimalMinute}분 ${decimalSecond}초 남았습니다.`;
+function setBackground() {
+  const gradientStart = colors[Math.floor(Math.random() * colors.length)];
+  const gradientEnd = colors[Math.floor(Math.random() * colors.length)];
+  const bg = document.querySelector("body");
+  bg.style.background = `linear-gradient(90deg, ${gradientStart}, ${gradientEnd})`;
 }
 
-getClock();
-setInterval(getClock, 1000);
+setBackground();
+button.addEventListener("click", setBackground);
